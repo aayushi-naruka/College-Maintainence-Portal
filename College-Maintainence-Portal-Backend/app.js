6031020460130c96ae729e44efc26295f02d0015
aayushi-naruka <narukaayushi02@gmail.com>
2023-04-25 19:39:22 +0530

    Backend structure and created Authentication APIs

--- College-Maintainence-Portal-Backend/app.js
+++ College-Maintainence-Portal-Backend/app.js
@@ -1,1 +1,27 @@
-ECHO is on.
+const express=require('express')
+const app=express()
+const authRoutes=require('./Routes/authRoutes')
+const PORT=3500
+const cors=require('cors')
+const connectToDb=require('./connect/MongooseConnect')
+
+const databaseName = 'mongodb+srv://narukaayushi02:IvOl3TUBXUfOLuSG@cluster0.rfpilfr.mongodb.net/?retryWrites=true&w=majority'
+
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/auth', authRoutes)
+
+async function mongoConnect() {
+    await connectToDb(databaseName)
+        .then((result) => {
+            console.log(result)
+            app.listen(PORT, () => {
+                console.log('Server started on port ' + 'http://localhost:' + PORT);
+            });
+        })
+}
+
+mongoConnect();  
